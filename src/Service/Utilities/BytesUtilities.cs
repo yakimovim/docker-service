e5f1fb8fa02dc9e291e58ec7a44d68524722ed3a
Ivan Yakimov <edlinsoft@gmail.com>
2021-09-19 19:18:40 +0300

    Presentation of size in bytes

---
+++ src/Service/Utilities/BytesUtilities.cs
@@ -0,0 +1,25 @@
+﻿namespace Service.Utilities
+{
+    public class BytesUtilities
+    {
+        public static string Simplify(long sizeInBytes)
+        {
+            var suffixes = new[] { "KB", "MB", "GB", "TB" };
+
+            var result = sizeInBytes.ToString();
+
+            var size = (double)sizeInBytes;
+
+            for (int i = 0; i < suffixes.Length; i++)
+            {
+                if (size < 1024.0) break;
+
+                size = size / 1024.0;
+
+                result = size.ToString("0.00") + " " + suffixes[i];
+            }
+
+            return result;
+        }
+    }
+}
